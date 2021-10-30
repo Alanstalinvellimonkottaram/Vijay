16fe2b8d046169e2783e37bb39ca9c89294a4988
Alanstalinvellimonkottaram <93390993+Alanstalinvellimonkottaram@users.noreply.github.com>
2021-10-30 14:06:52 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -112,7 +112,7 @@ async def start(bot, message):
             )
         )
     else:
-         await message.reply_text(
+        await message.reply_text(
             START_MSG.format(message.from_user.first_name),
             parse_mode="Markdown",
             disable_web_page_preview=True,
@@ -125,7 +125,7 @@ async def start(bot, message):
                             ),
                         InlineKeyboardButton
                             (
-                                "🍿MAIN CHANNEL🍿", url="https://t.me/joinchat/ISZ9R5CdkgBlMzNl"
+                                "🤖More Botz", url="https://t.me/MT_Botz"
                             )
                     ],
                     [
